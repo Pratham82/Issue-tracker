90f43cf99cdd39b6d7fd6078c5bbac8a1efa9b94
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-14 18:12:55 +0530

    Added all components to App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
 import SearchBar from './components/layout/SearchBar'
 import Logs from './components/logs/Logs'
+import AddBtn from './components/layout/AddBtn'
+import AddLogModal from './components/logs/AddLogModal'
 
 const App = () => {
   useEffect(() => {
@@ -14,6 +16,8 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
