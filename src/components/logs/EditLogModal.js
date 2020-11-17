a43fdf97d2023d15f7f4daa61edb22e952157e30
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-17 21:03:10 +0530

    Added update and delete functionality to the components

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,19 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import M from 'materialize-css/dist/js/materialize.min.js'
-const EditLogModal = () => {
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { updateLog } from '../../actions/logActions'
+
+const EditLogModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState('')
   const [attention, setAttention] = useState(false)
   const [tech, setTech] = useState('')
+
+  useEffect(() => {
+    if (current) {
+      setMessage(current.message)
+      setAttention(current.attention)
+      setTech(current.tech)
+    }
+  }, [current])
 
   const onSubmit = () => {
     if (message === '' || tech === '') {
       M.toast({ html: 'Please enter a message' })
     } else {
       console.log(message, tech, attention)
+
+      const updLog = {
+        id: current.id,
+        message,
+        attention,
+        tech,
+        date: new Date(),
+      }
+
+      updateLog(updLog)
+      M.toast({ html: `Log updated by ${tech}` })
+
+      setMessage('')
+      setAttention(false)
+      setTech('')
     }
-    setMessage('')
-    setAttention(false)
-    setTech('')
   }
 
   return (
@@ -60,7 +84,7 @@ const EditLogModal = () => {
                   type="checkbox"
                   className="filled-in"
                   value={attention}
-                  onChange={() => setAttention(!attention)}
+                  onChange={e => setAttention(!attention)}
                 />
                 <span> Needs attention</span>
               </label>
@@ -82,8 +106,18 @@ const EditLogModal = () => {
   )
 }
 
+EditLogModal.propTypes = {
+  updateLog: PropTypes.func.isRequired,
+  current: PropTypes.object,
+}
+
 const modalStyle = {
   width: '75%',
   height: '75%',
 }
-export default EditLogModal
+
+const mapStateToProps = state => ({
+  current: state.log.current,
+})
+
+export default connect(mapStateToProps, { updateLog })(EditLogModal)
--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,28 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment'
+import { connect } from 'react-redux'
+import { deleteLog, setCurrent } from '../../actions/logActions'
+import M from 'materialize-css/dist/js/materialize.min.js'
 
-const LogItem = ({ log: { id, message, attention, tech, date } }) => {
+const LogItem = ({
+  //log: { id, message, attention, tech, date },
+  deleteLog,
+  setCurrent,
+  log,
+}) => {
+  const onDelete = () => {
+    deleteLog(log.id)
+    M.toast({ html: 'Log deleted' })
+  }
   return (
     <li className="collection-item">
       <div>
         <a
           href="#edit-log-modal"
-          className={`modal-trigger ${attention ? 'red-text' : 'blue-text'}`}
+          className={`modal-trigger ${
+            log.attention ? 'red-text' : 'blue-text'
+          }`}
+          onClick={() => setCurrent(log)}
         >
-          {message}
+          {log.message}
         </a>
         <br />
         <span className="grey-text">
-          <span className="black-text">ID #{id}</span>
+          <span className="black-text">ID #{log.id}</span>
           &nbsp; last updated by &nbsp;{' '}
-          <span className="black-text">{tech}</span>
+          <span className="black-text">{log.tech}</span>
           &nbsp;on &nbsp;
           <Moment format="MMMM Do YYYY, h:mm:ss a" parse="YYYY-MM-DD HH:mm">
-            {date}
+            {log.date}
           </Moment>
         </span>
-        <a href="!#" className="secondary-content">
+        <a href="!#" className="secondary-content" onClick={onDelete}>
           <i className="material-icons grey-text">delete</i>{' '}
         </a>
       </div>
@@ -32,5 +47,7 @@ const LogItem = ({ log: { id, message, attention, tech, date } }) => {
 
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired,
+  setCurrent: PropTypes.func.isRequired,
 }
-export default LogItem
+export default connect(null, { deleteLog, setCurrent })(LogItem)
