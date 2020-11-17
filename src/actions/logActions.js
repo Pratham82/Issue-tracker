a679a0ee816192fefad5293555c00e7ffd208383
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-17 21:02:41 +0530

    Added update and delete functionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,13 @@
-import { GET_LOGS, ADD_LOG, SET_LOADING, LOGS_ERROR } from './types'
+import {
+  GET_LOGS,
+  ADD_LOG,
+  SET_LOADING,
+  LOGS_ERROR,
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LOG,
+} from './types'
 
 /*
 export const getLogs = () => {
@@ -59,6 +68,64 @@ export const addLog = log => async dispatch => {
       payload: err.response.data,
     })
   }
+}
+
+// Delete log from server
+export const deleteLog = id => async dispatch => {
+  try {
+    setLoading()
+    await fetch(`/logs/${id}`, {
+      method: 'DELETE',
+    })
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id,
+    })
+  } catch (err) {
+    /* handle error */
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    })
+  }
+}
+
+// Update log
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading()
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    const data = await res.json()
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data,
+    })
+  } catch (err) {
+    /* handle error */
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    })
+  }
+}
+
+// Set current Log
+export const setCurrent = log => {
+  return { type: SET_CURRENT, payload: log }
+}
+
+//Clear current Log
+export const clearCurrent = () => {
+  return { type: CLEAR_CURRENT }
 }
 
 export const setLoading = () => {
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,13 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from '../actions/types'
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LOG,
+} from '../actions/types'
 
 const initialState = {
   logs: null,
@@ -21,11 +30,31 @@ export default function logReducer(state = initialState, action) {
         logs: [...state.logs, action.payload],
         loading: false,
       }
+    case DELETE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter(log => log.id !== action.payload),
+        loading: false,
+      }
+    case UPDATE_LOG:
+      return {
+        ...state,
+        logs: state.logs.map(log =>
+          log.id === action.payload.id ? action.payload : log
+        ),
+      }
     case SET_LOADING:
       return {
         ...state,
         loading: true,
       }
+
+    case SET_CURRENT:
+      return { ...state, current: action.payload }
+
+    case CLEAR_CURRENT:
+      return { ...state, current: null }
+
     case LOGS_ERROR:
       console.error(action.payload)
       return {
