933d854d9d843ee3a906f0f2f8263df59cdb1cdb
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-18 12:16:18 +0530

    Added search functionality to Search bar

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { connect } from 'react-redux'
+import { searchLogs } from '../../actions/logActions'
+import PropTypes from 'prop-types'
 
-const SearchBar = () => {
+const SearchBar = ({ searchLogs }) => {
+  const text = useRef('')
+  const onChange = () => searchLogs(text.current.value)
   return (
     <nav style={{ marginBottom: '30px' }} className="blue">
       <div className="nav-wrapper">
         <form>
           <div className="input-field">
-            <input id="search" type="search" />
+            <input
+              id="search"
+              type="search"
+              placeholder="Search logs"
+              ref={text}
+              onChange={onChange}
+            />
             <label className="label-icon" htmlFor="search">
               <i className="material-icons">search</i>
             </label>
@@ -17,4 +28,9 @@ const SearchBar = () => {
     </nav>
   )
 }
-export default SearchBar
+
+SearchBar.propTypes = {
+  searchLogs: PropTypes.func.isRequired,
+}
+
+export default connect(null, { searchLogs })(SearchBar)
