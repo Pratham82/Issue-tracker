a570fbb11b997302c77fafc99d29ac85a4715da6
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-18 16:37:31 +0530

    Added new reducerc

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import logReducer from './logReducer'
+import techReucer from './techReducer'
 
 export default combineReducers({
   log: logReducer,
+  tech: techReucer,
 })
